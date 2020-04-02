82c5adeba78a7846339f30d801b299a213c95125
luckuy0 <luckuy369@gmail.com>
2020-04-02 21:15:00 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+footer {
+  position: fixed;
+  bottom: 0;
+  left:0;
+  width: 100%;
+  text-align: center;`
+  background-color: none;
+  text-align: center;
+  color: white;
+}
+</style>
+<style>
+.glow {
+  font-size: 40px;
+  color: #fff;
+  text-align: center;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+  }
+  
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+  }
+}
+</style>
+</head>
+<body>
+
+
+
+<footer>
+  <img src="https://madewithlove.now.sh/in?heart=true&colorA=%23303655&template=for-the-badge" alt="Made with love in India">
+</footer>
+</body>
+</html
